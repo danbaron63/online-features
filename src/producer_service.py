60dff98a68461d316c9723ff4ade165ed35a24e3
Dan Baron <dan.baron@starlingbank.com>
2024-07-11 17:57:35 +0100

    added timeout to producer to reduce hang when kafka is not available

--- src/producer_service.py
+++ src/producer_service.py
@@ -15,13 +15,14 @@ logger = logging.getLogger(__name__)
 host = os.environ.get("KAFKA_HOST", "localhost")
 conf = {
     'bootstrap.servers': f'{host}:9094,{host}:9092',
-    'client.id': socket.gethostname()}
+    'client.id': socket.gethostname(),
+    'message.timeout.ms': '5000'
+}
 logger.info("running with this config: %s", conf)
 
 producer = Producer(conf)
 
 
-# Produce a message
 def delivery_report(err, msg):
     if err is not None:
         logger.error(f"Message delivery failed: {err}")
